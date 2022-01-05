84f09daa1da94d8c3ffa9ed32b9ed9e0bb7b4374
danghieu1407 <danghieu140701@gmail.com>
2022-01-05 20:51:27 +0700

    hossting wweb lan 1

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const connectionString =  "mongodb://mongo:DuPgfv5xCXgQSY3P7ZME@containers-us-west-21.railway.app:7297"
+const connectionString =  "mongodb://mongo:cYt2QhLtqyfAmTvuRlpp@containers-us-west-22.railway.app:6391"
 if (!connectionString){
     console.error('MongoDB connection string missing!')
     process.exit(1)
